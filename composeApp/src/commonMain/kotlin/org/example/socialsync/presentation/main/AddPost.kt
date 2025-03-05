6e9f171b5d35c0849e8e6b7de5bbe03ff9e0143e
Ayush Sharma <165563915+iamayushsharmaa@users.noreply.github.com>
2025-03-05 20:01:26 +0530

    post

--- composeApp/src/commonMain/kotlin/org/example/socialsync/presentation/main/AddPost.kt
+++ composeApp/src/commonMain/kotlin/org/example/socialsync/presentation/main/AddPost.kt
@@ -13,11 +13,8 @@ import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import org.example.socialsync.app.AppColor
@@ -28,7 +25,6 @@ import org.jetbrains.compose.resources.painterResource
 
 @Composable
 fun AddPost(navController: NavHostController) {
-
     Column (
         modifier = Modifier
             .fillMaxSize()
@@ -66,7 +62,6 @@ fun AddPost(navController: NavHostController) {
         )
 
         Spacer(Modifier.height(10.dp))
-
         AttachmentRow(
             modifier = Modifier
                 .fillMaxWidth()
--- composeApp/src/commonMain/kotlin/org/example/socialsync/presentation/main/component/AttachmentRow.kt
+++ composeApp/src/commonMain/kotlin/org/example/socialsync/presentation/main/component/AttachmentRow.kt
@@ -1,20 +1,16 @@
 package org.example.socialsync.presentation.main.component
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import org.example.socialsync.app.AppColor
@@ -41,7 +37,7 @@ fun AttachmentRow(
                 tint = AppColor.Black,
                 modifier = Modifier.size(24.dp)
             )
-            Spacer(Modifier.width(8.dp))
+            Spacer(Modifier.width(6.dp))
             Text(
                 text = "Attach",
                 fontSize = 12.sp,
@@ -49,7 +45,6 @@ fun AttachmentRow(
                 modifier = Modifier.padding(top = 4.dp)
             )
         }
-
         Row (
             verticalAlignment = CenterVertically
         ) {
@@ -59,7 +54,7 @@ fun AttachmentRow(
                 tint = AppColor.Black,
                 modifier = Modifier.size(24.dp)
             )
-            Spacer(Modifier.width(8.dp))
+            Spacer(Modifier.width(6.dp))
 
             Text(
                 text = "File",
@@ -68,7 +63,6 @@ fun AttachmentRow(
                 modifier = Modifier.padding(top = 4.dp)
             )
         }
-
         Row(
             verticalAlignment = CenterVertically
         ) {
@@ -78,8 +72,7 @@ fun AttachmentRow(
                 tint = AppColor.Black,
                 modifier = Modifier.size(24.dp)
             )
-            Spacer(Modifier.width(8.dp))
-
+            Spacer(Modifier.width(6.dp))
             Text(
                 text = "Tag",
                 fontSize = 12.sp,
