bf58e9a97bb4caf5fdde36fde77ab57aac3f7684
Ayush Sharma <165563915+iamayushsharmaa@users.noreply.github.com>
2025-03-10 14:06:12 +0530

    Drafts

--- composeApp/src/commonMain/kotlin/org/example/socialsync/presentation/main/Setting.kt
+++ composeApp/src/commonMain/kotlin/org/example/socialsync/presentation/main/Setting.kt
@@ -1,9 +1,39 @@
 package org.example.socialsync.presentation.main
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 
 @Composable
 fun Setting(navController: NavHostController) {
 
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize(),
+        topBar = {
+            TopAppBar(
+                title = "Settings",
+            )
+        }
+    ) {
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .statusBarsPadding()
+                .padding(horizontal = 16.dp)
+        ){
+
+        }
+    }
 }
