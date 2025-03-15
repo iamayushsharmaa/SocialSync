dc8ed62f433ebec16d7bcdd7df8700d3c4ea858e
Ayush Sharma <165563915+iamayushsharmaa@users.noreply.github.com>
2025-03-15 15:18:33 +0530

    Design

--- composeApp/src/commonMain/kotlin/org/example/socialsync/presentation/main/PostDetailScreen.kt
+++ composeApp/src/commonMain/kotlin/org/example/socialsync/presentation/main/PostDetailScreen.kt
@@ -1,9 +1,12 @@
 package org.example.socialsync.presentation.main
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
@@ -50,18 +53,29 @@ fun PostDetailScreen (
                 .padding(it)
         ){
             Text(
+                text = "Connections",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Medium
+            )
+            Spacer(Modifier.height(10.dp))
+            Text(
                 text = "Text",
                 fontSize = 18.sp,
                 fontWeight = FontWeight.Medium
             )
             Text(
-                text = "Text",
+                text = "Caption...",
                 fontSize = 16.sp,
                 fontWeight = FontWeight.Normal,
                 modifier = Modifier
                     .fillMaxWidth()
                     .wrapContentHeight()
             )
+//            Image(
+//                painter = painterResource(),
+//                contentDescription = "",
+//
+//            )
         }
     }
 }
