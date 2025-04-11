119d095206a55221434714037793929d203223b2
Ayush Sharma <165563915+iamayushsharmaa@users.noreply.github.com>
2025-04-11 15:57:03 +0530

    Design

--- composeApp/src/commonMain/kotlin/org/example/socialsync/presentation/socials/ConnectButton.kt
+++ composeApp/src/commonMain/kotlin/org/example/socialsync/presentation/socials/ConnectButton.kt
@@ -1,15 +1,10 @@
 package org.example.socialsync.presentation.socials
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
